4c414d95fa0b5c4d4b66578bbd5e9389cece5042
Jordan Smith <jblsmith@gmail.com>
2021-01-30 14:39:50 +0000

    Able to set numbers (and set shade to white when doing so)

--- solve_kurodoko.py
+++ solve_kurodoko.py
@@ -6,8 +6,20 @@ class Kurodoko(object):
         
         assert len(grid_size)==2
         self.numbers = np.zeros(grid_size)
+        # Numbers are 0 if unset, else >= 1
         self.shades = np.zeros(grid_size)
+        # Sahdes are 0 if blank, 1 for white and -1 for black
         self.visible_in_row = np.zeros(grid_size)
         self.visible_in_column = np.zeros(grid_size)
     
+    def set_number(self, row, col, number):
+        assert self.numbers[row, col] == 0  # Only can set numbers in blank cells
+        assert self.shades[row, col] == 0   # ... and if cell is blank.
+        assert type(number) is int
+        self.numbers[row, col] = number
+        self.shades[row, col] = 1
+    
+    def set_numbers(self, assignment_list):
+        for row,col,number in assignment_list:
+            self.set_number(row, col, number)
     
--- test_kurodoko.py
+++ test_kurodoko.py
@@ -1,7 +1,7 @@
 # Requirements:
-# - grid of m x n cells
-# - all cells initially blank
-# - some cells initially designated with digits and are white
+# x grid of m x n cells
+# x all cells initially blank
+# x some cells initially designated with digits and are white
 # - cells can become black
 # - cells can become white
 # - detect whether space of white cells is contiguous
@@ -25,3 +25,17 @@ def test_make_grid():
 def test_grid_initially_blank():
     grid = Kurodoko((5,5))
     assert np.all(grid.numbers == 0)
+
+def test_set_number():
+    grid = Kurodoko((5,5))
+    grid.set_number(0,0,9)
+    assert grid.numbers[0,0] == 9
+
+def test_set_grid():
+    grid = Kurodoko((5,5))
+    grid.set_numbers([(0,0,9), (1,1,3), (1,2,7)])
+    assert grid.numbers[0,0] == 9
+    assert grid.numbers[1,1] == 3
+    assert grid.numbers[0,1] == 0
+    assert grid.shades[0,0] == grid.shades[1,1] == 1
+    assert grid.shades[0,1] == 0
