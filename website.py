513316274c5a69126c3ec7a339f7ce414c67720a
Jordan Smith <jblsmith@gmail.com>
2023-08-29 16:33:04 +0100

    Changes to website

--- website.py
+++ website.py
@@ -1,32 +1,39 @@
 import datetime
 from flask import Flask, render_template
-import numpy as np
 app = Flask(__name__)
+import os
 
-from solve_kurodoko import Kurodoko, make_kurodoko_from_file
+class MinimalKurodoko(object):
+    def __init__(self, grid_size, numbers):
+        self.grid_size = grid_size
+        self.numbers = numbers
 
-grid = make_kurodoko_from_file("./grids/7x7-01.csv")
-outcome = grid.solve_grid_with_deductions_and_single_conjectures(branched=True)
-solution = " ".join(np.concatenate(grid.shades).astype(str))
+def load_kurodoko_from_file(filepath):
+    with open(filepath) as textfile:
+        text = textfile.readlines()
+    text_lines = [line.strip().split(",") for line in text]
+    grid_size = (len(text_lines), len(text_lines[0]))
+    return MinimalKurodoko(grid_size, text_lines)
 
-
+def get_grid_and_solution_for_puzzle(puzzle_name):
+    grid = load_kurodoko_from_file(f"./grids/{puzzle_name}.csv")
+    solution_grid = load_kurodoko_from_file(f"./grids/{puzzle_name}-solution.csv")
+    solution = " ".join([" ".join(item) for item in solution_grid.numbers])
+    return grid, solution
 
 @app.route('/')
 def demo_puzzle():
-    return render_template('index.html', grid=grid, solution=solution)
-
-
-puzzle_names = ['7x7-01','7x7-02','7x7-03','7x7-04']
-puzzle_links = {name:next_name for name,next_name in zip(puzzle_names, puzzle_names[1:]+puzzle_names[:1])}
+    grid, solution = get_grid_and_solution_for_puzzle(puzzle_name="7x7-01")
+    return render_template('index.html', grid=grid, solution=solution, next_name="puzzle_7x7-02")
 
 @app.route('/puzzle_<name>')
 def show_puzzle(name):
-    assert name in puzzle_names
+    puzzle_names = ['7x7-01','7x7-02','7x7-03','7x7-04','7x7-05']
+    puzzle_links = {name:next_name for name,next_name in zip(puzzle_names, puzzle_names[1:]+puzzle_names[:1])}
+    asset_path_name = f"./grids/{name}-solution.csv"
+    assert os.path.exists(asset_path_name)
     next_name = "puzzle_" + puzzle_links[name]
-    filename = "./grids/"+name+".csv"
-    grid = make_kurodoko_from_file(filename)
-    grid.solve_grid_with_deductions_and_single_conjectures(branched=True)
-    solution = " ".join(np.concatenate(grid.shades).astype(str))
+    grid, solution = get_grid_and_solution_for_puzzle(puzzle_name=name)
     return render_template('index.html', grid=grid, solution=solution, next_name=next_name)
 
 
